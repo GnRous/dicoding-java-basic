aae570638d4331d3d69f8f59fca4e3a23e15ad43
Darmawan Setiawan <87440470+GnRous@users.noreply.github.com>
2024-02-29 23:40:13 +0800

    add array
    Menambahkan folder array

---
+++ Array/src/com/dicoding/javafundamental/array/InisiasiArray.java
@@ -0,0 +1,46 @@
+package com.dicoding.javafundamental.array;
+
+public class InisiasiArray {
+//    Cara Pertama
+//    public static void main(String[] args) {
+//        int[] arrInt = {1, 2, 3, 4, 5, 6};
+//
+//        System.out.println(arrInt[0]);
+//        System.out.println(arrInt[1]);
+//        System.out.println(arrInt[2]);
+//        System.out.println(arrInt[3]);
+//        System.out.println(arrInt[4]);
+//        System.out.println(arrInt[5]);
+//    }
+
+
+//    Cara Kedua
+//    public static void main(String[] args) {
+//        int[] arrInt = new int[6];
+//        arrInt[0] = 1;
+//        arrInt[1] = 2;
+//        arrInt[2] = 3;
+//        arrInt[3] = 4;
+//        arrInt[4] = 5;
+//        arrInt[5] = 6;
+//
+//        System.out.println(arrInt[0]);
+//        System.out.println(arrInt[1]);
+//        System.out.println(arrInt[2]);
+//        System.out.println(arrInt[3]);
+//        System.out.println(arrInt[4]);
+//        System.out.println(arrInt[5]);
+//    }
+
+//==========================================================
+
+    public static void main(String[] args) {
+        int[] arrInt = {1, 2, 3, 4, 5, 6};
+
+        for (int x = 0; x < arrInt.length; x++) {
+            arrInt[x] = x + 1;
+            System.out.println(arrInt[x]);
+        }
+    }
+
+}
