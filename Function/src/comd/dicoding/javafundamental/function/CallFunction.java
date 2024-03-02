051db5787cebe8cacfb96baece74e7ca189d9133
Darmawan Setiawan <87440470+GnRous@users.noreply.github.com>
2024-03-02 21:54:28 +0800

    added Function code

---
+++ Function/src/comd/dicoding/javafundamental/function/CallFunction.java
@@ -0,0 +1,12 @@
+package comd.dicoding.javafundamental.function;
+
+public class CallFunction {
+    public static void main(String[] args) {
+        // memanggil fungsi
+        cobaFungsi("Yang aku tahu");
+    }
+
+    public static void cobaFungsi(String name) {
+        System.out.println("Ini adalah bagian dari fungsi " + name);
+    }
+}
