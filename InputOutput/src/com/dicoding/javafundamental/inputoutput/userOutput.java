7f4a58ebf800184258bd063d0fb8b16aeacf4e53
Darmawan Setiawan <87440470+GnRous@users.noreply.github.com>
2024-03-02 21:57:06 +0800

    added InputOutput code

---
+++ InputOutput/src/com/dicoding/javafundamental/inputoutput/userOutput.java
@@ -0,0 +1,11 @@
+package com.dicoding.javafundamental.inputoutput;
+
+public class userOutput {
+
+    public static void main(String[] args) {
+        System.out.println("Dicetak pakai println()");
+        System.out.println("ini juga Dicetak pakai println()");
+        System.out.print("ini dicetak dengan print()");
+        System.out.print("dan ini juga dicetak dengan print()");
+    }
+}
